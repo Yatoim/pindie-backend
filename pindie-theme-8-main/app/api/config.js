90e3760596c39ef4056cf37e7654ab3ef34ad7be
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 14:09:55 +0300

    Add files via upload

---
+++ pindie-theme-8-main/app/api/config.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'http://pindie-moy-proect.nomorepartiesco.ru/'
+
+export const endpoints = {
+  games: `${BASE_URL}/api/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+}
