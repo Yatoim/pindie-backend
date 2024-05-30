7e6bb5b5f1c73eb6067c85dc5837412a1ff0727b
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 15:08:38 +0300

    Update app.js

--- pindie-backend/app.js
+++ pindie-backend/app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const apiRouter = require ('./routes/apiRouter')
-const cookieParser = require("cookie-parser");
-const connectToDatabase = require('./database/connect');
-const cors = require('./middlewares/cors');
-const pagesRouter = require ('./routes/pages');
-const authRouter = require('./routes/auth');
-
+const express = require("express");
 const app = express();
+const cors = require("./middlewares/cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectToDatabase = require("./database/connect");
+
+const apiRouter = require("./routes/apiRouter");
+const pagesRouter = require("./routes/pages");
+const usersRouter = require("./routes/users");
+const gamesRouter = require("./routes/games");
+const categoriesRouter = require("./routes/categories");
+const cookieParser = require("cookie-parser");
+
 const PORT = 3001;
 
 connectToDatabase();
@@ -17,11 +20,12 @@ app.use(
   cors,
   cookieParser(),
   bodyParser.json(),
-  pagesRouter, // Добавляем роутер для страниц
+  pagesRouter,
   apiRouter,
-  authRouter,
-  express.static(path.join(__dirname, "public"))
+  express.static(path.join(__dirname, "/public"))
 );
- 
-app.listen(PORT);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
  
