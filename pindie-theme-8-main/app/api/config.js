29e5e052f360c958057299f058c442ff7616704e
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 17:28:34 +0300

    Update config.js

--- pindie-theme-8-main/app/api/config.js
+++ pindie-theme-8-main/app/api/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://pindie-test-backend.nomorepartiesco.ru/api'
-
+mode: 'no-cors'
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
