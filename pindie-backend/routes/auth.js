42f4752e240b7504c360f2c736095488f0479467
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 15:07:10 +0300

    Update auth.js

--- pindie-backend/routes/auth.js
+++ pindie-backend/routes/auth.js
@@ -1,8 +1,6 @@
-// routes/auth.js
-
 const authRouter = require("express").Router();
-const { login, sendIndex, sendDashboard} = require("../controllers/auth.js");
+const {login, sendIndex} = require("../controllers/auth.js");
 
-authRouter.post("/api/auth/login", login, sendIndex, sendDashboard);
+authRouter.post("/auth/login", login);
 
 module.exports = authRouter;
