01aee3bcf1a300db076a92588bba78e71f004b2b
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 14:54:11 +0300

    Update pages.js

--- pindie-backend/routes/pages.js
+++ pindie-backend/routes/pages.js
@@ -1,11 +1,8 @@
-// routes/pages.js
 const pagesRouter = require("express").Router();
-const { sendIndex, sendDashboard } = require("../controllers/auth.js");
-const {checkCookiesJWT, checkAuth} = require('../middlewares/auth.js')
-pagesRouter.get("/", sendIndex);
-// routes/pages.js
-// Импорты и другие маршруты
+const { login, sendIndex, sendDashboard } = require("../controllers/auth.js");
+const { checkAuth, checkCookiesJWT } = require("../middlewares/auth.js");
 
-pagesRouter.get("/admin/**", sendDashboard);
-pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard); 
-module.exports = pagesRouter
+pagesRouter.get("/", sendIndex);
+pagesRouter.get("/admin/**", checkCookiesJWT, checkAuth, sendDashboard);
+
+module.exports = pagesRouter;
