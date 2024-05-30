b136699d51d6134a54476a6ff24ffc237c97bfeb
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 14:24:15 +0300

    Delete pindie-backend directory

--- pindie-backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-// routes/auth.js
-
-const authRouter = require("express").Router();
-const { login, sendIndex, sendDashboard} = require("../controllers/auth.js");
-
-authRouter.post("/api/auth/login", login, sendIndex, sendDashboard);
-
-module.exports = authRouter;
