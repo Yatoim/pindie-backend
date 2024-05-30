955c6778b46adf5cde3b5dd390dad66daac3678a
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 16:14:32 +0300

    Update login.js

--- pindie-backend/public/scripts/login.js
+++ pindie-backend/public/scripts/login.js
@@ -1,5 +1,5 @@
 import { showTooltip } from "./dom-creators.js";
-const bcryptjs = require("bcryptjs");
+import bcryptjs from 'bcryptjs';
 const form = document.querySelector(".auth-form");
 form &&
   form.addEventListener("submit", async event => {
