1829819fa78461925e6f5ed2f72a6b337c3868c1
Yatoim <149510305+Yatoim@users.noreply.github.com>
2024-05-30 14:09:45 +0300

    Delete pindie-theme-8-main/app/api/config.js

--- pindie-theme-8-main/app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'http://pindie-moy-proect.nomorepartiesco.ru/'
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/me`,
-}
